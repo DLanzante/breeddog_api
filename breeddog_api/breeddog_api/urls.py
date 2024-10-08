1eb278abc38fd61d9544aef1792c6eb6427510bb
Dominic Lanzante <dominiclanzante@gmail.com>
2024-10-07 19:42:09 -0500

    This is new changes to the api/dogs

--- breeddog_api/breeddog_api/urls.py
+++ breeddog_api/breeddog_api/urls.py
@@ -20,7 +20,7 @@ from dogapi.views import DogList, DogDetail, BreedList, BreedDetail
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('dogs/', DogList.as_view(), name='dog-list'),
+    path('api/dogs/', DogList.as_view(), name='dog-list'),
     path('dogs/<int:pk>/', DogDetail.as_view(), name='dog-detail'),
     path('breeds/', BreedList.as_view(), name='breed-list'),
     path('breeds/<int:pk>/', BreedDetail.as_view(), name='breed-detail'),
