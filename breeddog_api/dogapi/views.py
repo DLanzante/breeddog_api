1c9b21cf1dce30cab38d841a085e176860a57119
Dominic Lanzante <dominiclanzante@gmail.com>
2024-10-10 14:24:55 -0500

    These are my changes

--- breeddog_api/dogapi/views.py
+++ breeddog_api/dogapi/views.py
@@ -2,7 +2,6 @@ from rest_framework import generics
 from .models import Dog, Breed
 from .serializers import DogSerializer, BreedSerializer
 
-# Dog API Views
 class DogList(generics.ListCreateAPIView):
     queryset = Dog.objects.all()
     serializer_class = DogSerializer
@@ -11,7 +10,6 @@ class DogDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Dog.objects.all()
     serializer_class = DogSerializer
 
-# Breed API Views
 class BreedList(generics.ListCreateAPIView):
     queryset = Breed.objects.all()
     serializer_class = BreedSerializer
